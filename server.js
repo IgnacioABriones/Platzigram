a301549f7078eb65a663521f7dcd7cb04731d7aa
Ignacio Briones <i.briones.santander@gmail.com>
2017-07-24 23:32:19 -0400

    agregando vistas con pug(jade) motor de templates, creacion de fragmentos de HTML

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
+
 var app = express();
+
+app.set('view engine', 'pug');
+
 app.get('/', function(req, res){
-    res.send('hola mundo!');
+    res.render('index');
 })
 app.listen(3000,function(err){
    if(err) return console.log ('Hubo un error'), process.exit(1);
