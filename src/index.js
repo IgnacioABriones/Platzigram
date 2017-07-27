8b393845e8885045f2261eefd594c2b7ca8362d3
Ignacio Briones <i.briones.santander@gmail.com>
2017-07-27 17:00:33 -0400

    page.js incorporando navegacion a simple page application, rutas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert('Hola');
+var page = require('page');
+
+var main = document.getElementById('main-container')
+
+page('/', function(ctx, next){
+  main.innerHTML =  'Home <a href="/signup">Signup</a>';//diga home
+})
+
+
+page('/signup', function(ctx, next){
+  main.innerHTML = 'Signup <a href="/">Home</a>'; // diga signup
+})
+
+page('/signin', function(ctx, next){
+  main.innerHTML = 'signin'; // diga signup
+})
+
+
+page();
