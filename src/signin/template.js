b7039f6ffb38f543d10eaad87ea3393b095b59e8
Ignacio Briones <i.briones.santander@gmail.com>
2017-08-01 14:59:42 -0400

    creando modulos para el signin y el signup

---
+++ src/signin/template.js
@@ -0,0 +1,30 @@
+var yo = require('yo-yo');
+var landing = require('../landing');
+
+var signinForm = yo`<div class="col s12 m7">
+            <div class="row">
+              <div class="signup-box">
+                <h1 class="platzigram">Facebook</h1>
+                  <form class="signup-form" action="index.html" method="post">
+                       <div class="section">
+                        <a class="btn btn-fb hiden-on-smal-only">Iniciar Sesion con Facebook</a>
+                        <a class="btn btn-fb hide-on-med-and-up">Iniciar Sesion</a>
+                      </div>
+                      <div class="divider"></div>
+                        <div class="section">
+                          <input type="text" name="username" placeholder="Nombre de Usuario"/>
+                          <input type="password" name="password" placeholder="Contraseña"/>
+                          <button class="btn waves-effect waves-light btn-signup"type="submit" name="button">Inicia Sesion</button>
+                        </div>
+                    </form>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="login-box">
+                       ¿No Tienes una cuenta? <a href="/signup">Registrate</a>
+                    </div>
+                  </div>
+                </div>`;
+    
+
+module.exports = landing(signinForm);    
---
+++ src/signup/template.js
@@ -0,0 +1,33 @@
+var yo = require('yo-yo');
+var landing = require('../landing');
+
+var signupForm = yo`<div class="col s12 m7">
+            <div class="row">
+              <div class="signup-box">
+                <h1 class="platzigram">Integranet</h1>
+                  <form class="signup-form" action="index.html" method="post">
+                    <h2>Registrate para ver fotos de tus amigos estudiando en Platzi</h2>
+                      <div class="section">
+                        <a class="btn btn-fb hiden-on-smal-only">Iniciar Sesion con Facebook</a>
+                        <a class="btn btn-fb hide-on-med-and-up">Iniciar Sesion</a>
+                      </div>
+                      <div class="divider"></div>
+                        <div class="section">
+                          <input type="email" name="email" placeholder="correo electronico"/>
+                          <input type="text" name="name" placeholder="Nombre completo"/>
+                          <input type="text" name="username" placeholder="Nombre de Usuario"/>
+                          <input type="password" name="password" placeholder="Contraseña"/>
+                          <button class="btn waves-effect waves-light btn-signup"type="submit" name="button">Registrar</button>
+                        </div>
+                    </form>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="login-box">
+                       ¿Tienes una cuenta? <a href="/signin">Entrar</a>
+                    </div>
+                  </div>
+                </div>`;
+    
+
+module.exports = landing(signupForm);    
