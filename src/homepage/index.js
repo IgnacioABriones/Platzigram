4b7263d46b7f8d07e26a15c266435e41cb1be10f
Ignacio Briones <i.briones.santander@gmail.com>
2017-08-04 21:04:37 -0400

    Agregando las imágenes de los usuarios al timeline video 18

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -8,5 +8,28 @@ var title = require('title');
 page('/', function(ctx, next){
 	title('platzigram');
   var main = document.getElementById('main-container');
-  empty(main).appendChild(template);
-    })
+  
+  var pictures = [
+  	{
+  		user: { 
+  			username: 'ibriones',
+  			avatar: 'https://scontent.fscl14-1.fna.fbcdn.net/v/t1.0-1/p240x240/18425566_10213226037144284_911498844009859581_n.jpg?oh=b4571579ecf203c5289a53d6aab3c8ea&oe=59FF9237'
+  		},
+  		url: 'office.jpg',
+  		likes: 10,
+  		liked: true
+  	},
+  	{ 
+  		user: { 
+  			username: 'ibriones',
+  			avatar: 'https://scontent.fscl14-1.fna.fbcdn.net/v/t1.0-1/p240x240/18425566_10213226037144284_911498844009859581_n.jpg?oh=b4571579ecf203c5289a53d6aab3c8ea&oe=59FF9237'
+  		},
+  		url: 'office.jpg',
+  		likes: 2,
+  		liked: true
+  	}
+  ];
+
+
+  empty(main).appendChild(template(pictures));
+})
