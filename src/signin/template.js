2fa0f5e97f51014f0fbf6606984ffc268c555db2
Ignacio Briones <i.briones.santander@gmail.com>
2017-08-14 23:16:50 -0300

    cambiando el Idioma y alamacenandolo en el localStorage

--- src/signin/template.js
+++ src/signin/template.js
@@ -1,27 +1,28 @@
 var yo = require('yo-yo');
 var landing = require('../landing');
+var translate =  require('../translate')
 
 var signinForm = yo`<div class="col s12 m7">
             <div class="row">
               <div class="signup-box">
-                <h1 class="platzigram">Facebook</h1>
+                <h1 class="platzigram">Instagram</h1>
                   <form class="signup-form" action="index.html" method="post">
                        <div class="section">
-                        <a class="btn btn-fb hiden-on-smal-only">Iniciar Sesion con Facebook</a>
-                        <a class="btn btn-fb hide-on-med-and-up"><i class="fa fa-facebook-official"></i>Iniciar Sesion</a>
+                        <a class="btn btn-fb hiden-on-smal-only">${translate.message('signup.facebook')}</a>
+                        <a class="btn btn-fb hide-on-med-and-up"><i class="fa fa-facebook-official"></i>${translate.message('signup.text')}</a>
                       </div>
                       <div class="divider"></div>
                         <div class="section">
-                          <input type="text" name="username" placeholder="Nombre de Usuario"/>
-                          <input type="password" name="password" placeholder="Contraseña"/>
-                          <button class="btn waves-effect waves-light btn-signup"type="submit" name="button">Inicia Sesion</button>
+                          <input type="text" name="username" placeholder="${translate.message('username')}"/>
+                          <input type="password" name="password" placeholder="${translate.message('fullname')}"/>
+                          <button class="btn waves-effect waves-light btn-signup"type="submit" name="button">${translate.message('signup.text')}</button>
                         </div>
                     </form>
                     </div>
                   </div>
                   <div class="row">
                     <div class="login-box">
-                       ¿No Tienes una cuenta? <a href="/signup">Registrate</a>
+                       ${translate.message('signin.not-have-account')} <a href="/signup">${translate.message('signup.call-to-action')}</a>
                     </div>
                   </div>
                 </div>`;
--- src/translate/en-US.js
+++ src/translate/en-US.js
@@ -2,5 +2,20 @@ module.exports = {
   likes: '{ likes, plural, ' +
           '=0 { no likes }' +
           '=1 { # like }' +
-          'other { # likes }}'
+          'other { # likes }}',
+  		  'logout': 'Logout',
+  		  'english': 'English',
+  		  'spanish': 'Spanish',
+  		  'signup.subheading': 'Signup to watch your friend\'s picture studying in Platzi',
+  		  'signup.facebook': 'Signup with Facebook',
+  		  'signup.text': 'Signup',
+  		  'email': 'Email',
+  		  'username': 'username',
+  		  'fullname': 'fullname',
+  		  'password': 'password',
+  		  'signup.call-to-action': 'Signin',
+  		  'signup.have-account': 'Already have an account?',
+  		  'signin': 'Signin',
+ 		  'signin.not-have-account': '¿Don\'t have an account?',
+  		  'language': 'language'
 }
